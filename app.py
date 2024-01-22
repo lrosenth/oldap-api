44d8783eeb0784f57da021ffba7215d16204a867
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-01-22 16:35:40 +0100

    First tries
    omas librarby is working…

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from omaslib.src.connection import Connection
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    con = Connection(server='http://localhost:7200',
+                         repo="omas",
+                         userid="rosenth",
+                         credentials="RioGrande",
+                         context_name="DEFAULT")
+    return con.token
 
 if __name__ == '__main__':
     app.run()
