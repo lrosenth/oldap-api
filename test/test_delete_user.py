47d1f6319e273f51e019b42c254a57c149967cf9
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-03-18 17:55:25 +0100

    startet testing of delete user

---
+++ test/test_delete_user.py
@@ -0,0 +1,55 @@
+
+
+def test_delete_user(client, token_headers):
+    header = token_headers[1]
+
+    client.put('/admin/user/rosman', json={
+        "givenName": "Manuel",
+        "familyName": "Rosenthaler",
+        "password": "kappa1234",
+        "inProjects": [
+            {
+                "project": "http://www.salsah.org/version/2.0/SwissBritNet",
+                "permissions": [
+                    "ADMIN_USERS"
+                ]
+            }
+        ],
+        "hasPermissions": [
+            "GenericView"
+        ]
+    }, headers=header)
+
+    response = client.delete('/admin/user/rosman', headers=header)
+
+    res = response.json
+    assert res["message"] == "User rosman deleted"
+
+
+def test_delete_nonexisting_user(client, token_headers):
+    header = token_headers[1]
+
+    client.put('/admin/user/rosman', json={
+        "givenName": "Manuel",
+        "familyName": "Rosenthaler",
+        "password": "kappa1234",
+        "inProjects": [
+            {
+                "project": "http://www.salsah.org/version/2.0/SwissBritNet",
+                "permissions": [
+                    "ADMIN_USERS"
+                ]
+            }
+        ],
+        "hasPermissions": [
+            "GenericView"
+        ]
+    }, headers=header)
+
+    response = client.delete('/admin/user/kappa', headers=header)
+
+    res = response.json
+    assert res["message"] == 'User "kappa" not found.'
+
+    # Cleanup
+    client.delete('/admin/user/rosman', headers=header)
