22a2bd083e09bec713c8549d045bdb3a579d3636
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-03-25 16:06:54 +0100

    Added a bunch of new Tests

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -20,6 +20,24 @@ def test_modify_givenname(client, token_headers, testuser):
     assert readed["given_name"] == "Kappa"
 
 
+def test_bad_modify_givenname(client, token_headers, testuser):
+    # TODO: Dieser Test muss noch im Backend abgefangen werden
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "givenName": "gagagag<!$"  # TODO: Werden hier Sonderzeichen nicht rausgenommen?
+    }, headers=header)
+
+    assert response.status_code == 400
+    res = response.json
+    print(res)
+    assert res["message"] == "User updated successfully"
+
+    read = client.get('/admin/user/rosman', headers=header)
+    readed = read.json
+    assert readed["given_name"] == ""
+
+
 def test_modify_familyname(client, token_headers, testuser):
     header = token_headers[1]
 
@@ -75,10 +93,66 @@ def test_modify_inproject(client, token_headers, testuser):
     print(readed)
 
 
+def test_modify_empty_inproject(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "inProjects": []
+    }, headers=header)
+
+    res = response.json
+    assert response.status_code == 200
+
+    read = client.get('/admin/user/rosman', headers=header)
+    readed = read.json
+    print(readed)
+    assert readed["in_projects"] == []
+
+
+def test_modify_empty_inproject_permissions(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "inProjects": [
+            {
+                "project": "http://www.salsah.org/version/2.0/SwissBritNet",
+                "permissions": []
+            }
+        ]
+    }, headers=header)
+
+    assert response.status_code == 200
+    res = response.json
+
+    read = client.get('/admin/user/rosman', headers=header)
+    readed = read.json
+    assert readed["in_projects"][0]["permissions"] == []
+
+
+def test_modify_empty_inproject_name(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "inProjects": [
+            {
+                "project": "",
+                "permissions": ["ADMIN_RESOURCES"]
+            }
+        ]
+    }, headers=header)
+
+    assert response.status_code == 400
+    res = response.json
+
+    read = client.get('/admin/user/rosman', headers=header)
+    readed = read.json
+    assert readed["in_projects"][0]["permissions"] == ["omas:ADMIN_USERS"]
+    assert readed["in_projects"][0]["project"] == "http://www.salsah.org/version/2.0/SwissBritNet"
+
+
 def test_modify_haspermission(client, token_headers, testuser):
     header = token_headers[1]
 
-    # TODO: Bug! Wenn leere Liste übergeben wird dann bleiben die alten haspermissions erhalten
     response = client.post('/admin/user/rosman', json={
         "hasPermissions": [
             "GenericView"
@@ -91,3 +165,52 @@ def test_modify_haspermission(client, token_headers, testuser):
     read = client.get('/admin/user/rosman', headers=header)
     readed = read.json
     assert readed["has_permissions"] == ['omas:GenericView']
+
+
+def test_modify_empty_haspermission(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "hasPermissions": []
+    }, headers=header)
+
+    assert response.status_code == 200
+    res = response.json
+
+    read = client.get('/admin/user/rosman', headers=header)
+    readed = read.json
+    assert readed["has_permissions"] == []
+
+
+def test_bad_modify_haspermission(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "hasPermissions": ['Gagaga+++kappa']
+    }, headers=header)
+
+    assert response.status_code == 400
+    res = response.json
+    assert res["message"] == "The given permission is not a QName"
+
+
+def test_malicious_modify_haspermission(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={
+        "hasPermissions": ['Gagaga+++kappa<!$'] # TODO: wenn man Gagaga+++kappa<!$ eingibt dann kommt komischer error. Injection?
+    }, headers=header)
+
+    assert response.status_code == 400
+
+
+def test_bad_general_modify_request(client, token_headers, testuser):
+    header = token_headers[1]
+
+    response = client.post('/admin/user/rosman', json={'random shit': 'kappa'}, headers=header)
+
+    res = response.json
+    assert response.status_code == 400
+    assert res["message"] == "Either the firstname, lastname, password, inProjects or hasPermissions needs to be modified"
+
+
