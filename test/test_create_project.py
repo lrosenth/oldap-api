9d285abc6fd1f91c9c3763f01fb03447436dd7d3
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-04-10 16:22:57 +0200

    starting small testing for search project

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -70,3 +70,15 @@ def test_read_project(client, token_headers, testproject):
     assert response.status_code == 200
     res = response.json
     print(res)
+
+
+def test_search_project(client, token_headers, testproject):
+    header = token_headers[1]
+
+    response = client.get('/admin/project/search', json={
+        "label": "unittest"
+    }, headers=header)
+
+    res = response.json
+    print(res)
+
