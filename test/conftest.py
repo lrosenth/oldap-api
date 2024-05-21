3a64851458378de673d976aa873846a065b134df
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-05-21 18:09:39 +0200

    add new testpermissionsetfixture

--- test/conftest.py
+++ test/conftest.py
@@ -99,3 +99,19 @@ def testproject(client, token_headers):
 
     client.delete('/admin/project/testproject', headers=header)
 
+
+@pytest.fixture()
+def testpermissionset(client, token_headers):
+    header = token_headers[1]
+
+    response = client.put('/admin/permissionset/testpermissionset', json={
+        "label": ["testPerm@en", "test@Perm@de"],
+        "comment": ["For testing@en", "Für Tests@de"],
+        "givesPermission": "DATA_UPDATE",
+        "definedByProject": "omas:SystemProject"
+    }, headers=header)
+
+    yield
+
+    # client.delete('/admin/project/testproject', headers=header)
+
