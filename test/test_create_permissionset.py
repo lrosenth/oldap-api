3708be2abfb2c382571629f56c66281df4479280
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-05-27 17:28:10 +0200

    Additional tests for create_permissionset

--- test/test_create_permissionset.py
+++ test/test_create_permissionset.py
@@ -8,6 +8,20 @@ def test_create_permissionset(client, token_headers):
         "definedByProject": "omas:SystemProject"
     }, headers=header)
 
+    assert response.status_code == 200
+    res = response.json
+    print(res)
+
+
+def test_create_permissionset_with_empty_fields(client, token_headers):
+    header = token_headers[1]
+
+    response = client.put('/admin/permissionset/testpermissionset', json={
+        "comment": ["For testing@en", "Für Tests@de"],
+        "givesPermission": "DATA_UPDATE",
+        "definedByProject": "omas:SystemProject"
+    }, headers=header)
+
     # assert response.status_code == 200
     res = response.json
     print(res)
