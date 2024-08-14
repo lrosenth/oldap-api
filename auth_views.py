8aaf89511cd655dd349f8c00b976e0a665f53341
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-08-14 18:22:17 +0200

    Refactoring admin file into subcategories

---
+++ auth_views.py
@@ -0,0 +1,46 @@
+from flask import request, jsonify, Blueprint
+from oldaplib.src.connection import Connection
+from oldaplib.src.helpers.oldaperror import OldapErrorNotFound, OldapError
+
+auth_bp = Blueprint('auth', __name__, url_prefix='/admin')
+
+
+@auth_bp.route('/auth/<userid>', methods=['POST'])
+def login(userid):
+    """
+    Viewfunction to log into a user. A JSON is expected with the password. The userid is given via the URL parameter.
+    The JSON that needs to be provided has the following form: json={'password': 'RioGrande'}
+    :param userid: The userid of the loginaccount.
+    :return: A JSON with the token that has the following form:
+    json={'message': 'Login succeeded', 'token': token}
+    """
+    if request.is_json:
+        data = request.get_json()
+        password = data.get('password')
+        if password is None:
+            return jsonify({"message": "Invalid content type, JSON required"}), 400
+        try:
+            con = Connection(server='http://localhost:7200',
+                             repo="oldap",
+                             userId=userid,
+                             credentials=password,
+                             context_name="DEFAULT")
+            resp = jsonify({'message': 'Login succeeded', 'token': con.token}), 200
+            return resp
+        except OldapErrorNotFound as err:
+            return jsonify({'message': str(err)}), 404
+        except OldapError as error:
+            return jsonify({"message": f"Connection failed: {str(error)}"}), 403
+    else:
+        return jsonify({"message": f"JSON expected. Instead received {request.content_type}"}), 400
+
+
+@auth_bp.route('/auth/<userid>', methods=['DELETE'])
+def logout(userid):
+    """
+    Viewfunction to log out of a user.
+    :param userid: The userid of the logout account.
+    :return:
+    """
+    # TODO: how to make a logout??? oldaplib does not yet have a solution! So we just return 200
+    return '', 200
--- factory.py
+++ factory.py
@@ -5,6 +5,15 @@ def factory():
     app = Flask(__name__)
 
     import admin
+    import auth_views
+    import user_views
+    import project_views
+    import permset_views
+
     app.register_blueprint(admin.bp)
+    app.register_blueprint(auth_views.auth_bp)
+    app.register_blueprint(user_views.user_bp)
+    app.register_blueprint(project_views.project_bp)
+    app.register_blueprint(permset_views.permset_bp)
 
     return app
