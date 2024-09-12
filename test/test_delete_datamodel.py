9b0c2ed70e5fe975547090053c8f55df13b04711
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-09-12 16:38:36 +0200

    del testing

--- test/test_delete_datamodel.py
+++ test/test_delete_datamodel.py
@@ -11,3 +11,32 @@ def test_delete_standalone_property(client, token_headers, testfulldatamodelstan
 
     response = client.get('/admin/permissionset/testpermission', headers=header)
     assert response.status_code == 404
+
+
+def test_delete_resource(client, token_headers, testfulldatamodelresource):
+    header = token_headers[1]
+
+    response = client.delete('/admin/datamodel/hyha/hyha:Sheep/del', headers=header)
+
+    assert response.status_code == 200
+
+    res = response.json
+    print(res)
+    assert res['message'] == 'Data model successfully deleted'
+
+    response = client.get('/admin/permissionset/testpermission', headers=header)
+    assert response.status_code == 404
+
+def test_delete_property_in_resource(client, token_headers, testfulldatamodelresource):
+    header = token_headers[1]
+
+    response = client.delete('/admin/datamodel/hyha/hyha:Sheep/hyha:testProp2/del', headers=header)
+
+    assert response.status_code == 200
+
+    res = response.json
+    print(res)
+    assert res['message'] == 'Data model successfully deleted'
+
+    response = client.get('/admin/permissionset/testpermission', headers=header)
+    assert response.status_code == 404
