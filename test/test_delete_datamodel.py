e58385cebd46465078eaeb65d9f21ee052376dc3
ManuelRosenthaler <rosenthalermanuel@gmail.com>
2024-12-12 17:54:18 +0100

    more error handling for delete dm

--- test/test_delete_datamodel.py
+++ test/test_delete_datamodel.py
@@ -76,15 +76,16 @@ def test_bad_token(client, token_headers, testfulldatamodelstandaloneproplangstr
     modified_token = token + "kappa"
     header['Authorization'] = 'Bearer ' + modified_token
 
-    response = client.delete('/admin/datamodel/hyha/hyha:testProp2/del', headers=header)
-    assert response.status_code == 403
-    res = response.json
-    assert res["message"] == "Connection failed: Wrong credentials"
-
     response = client.delete('/admin/datamodel/hyha', headers=header)
     assert response.status_code == 403
     res = response.json
     assert res["message"] == "Connection failed: Wrong credentials"
+
+    response = client.delete('/admin/datamodel/hyha/hyha:Sheep', headers=header)
+    assert response.status_code == 403
+    res = response.json
+    assert res["message"] == "Connection failed: Wrong credentials"
+
 
 def test_cantfind_dm_to_delete(client, token_headers):
     header = token_headers[1]
@@ -116,6 +117,47 @@ def test_cantfind_standaloneprop(client, token_headers, testfulldatamodelstandal
     res = response.json
     print(res)
 
+def test_cantfind_resource_to_delete(client, token_headers, testfulldatamodelresource):
+    header = token_headers[1]
+
+    response = client.delete('/admin/datamodel/hyha/doesnotexist', headers=header)
+    assert response.status_code == 404
+
+    res = response.json
+    print(res)
+
+def test_cantfind_dm_to_delete_resource(client, token_headers, testfulldatamodelresource):
+    header = token_headers[1]
+
+    response = client.delete('/admin/datamodel/doesnotexist/doesnotexist', headers=header)
+    assert response.status_code == 404
+
+    res = response.json
+    print(res)
+
+def test_cantfind_dm_to_delete_hasprop(client, token_headers, testfulldatamodelresource):
+    header = token_headers[1]
+
+    response = client.delete('/admin/datamodel/doesnotexist/hyha:Sheep/hyha:testProp2', headers=header)
+    assert response.status_code == 404
+
+    res = response.json
+    print(res)
+
+def test_cantfind_hasprop_or_resource_to_delete(client, token_headers, testfulldatamodelresource):
+    header = token_headers[1]
+
+    response = client.delete('/admin/datamodel/hyha/doesnotexist/hyha:testProp2', headers=header)
+    assert response.status_code == 404
+
+    res = response.json
+    print(res)
+
+    response = client.delete('/admin/datamodel/hyha/hyha:Sheep/doesnotexist', headers=header)
+    assert response.status_code == 404
+
+    res = response.json
+    print(res)
 
 def test_bad_token_hasprop(client, token_headers, testfulldatamodelresource):
     header = token_headers[1]
